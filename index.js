e468dbaa78794f5659c68da4de7de527258b5924
Noura <contact.nouraloudani@gmail.com>
2021-01-14 12:39:24 -0500

    commit tree 348d9c0529623974d0ca93b7bcf8fe503646c380

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code goes here
+// // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    console.log("hi")
+    const p = document.querySelector('#text')
+    p.textContent = "This is really cool!"
+
+});
+//     console.log("The DOM has loaded");
+//   });
+  
+//   console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
